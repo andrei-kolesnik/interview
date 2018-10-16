06c9d49d428ecb3aaa268aa7084f86dfcf70dea6
andrei-kolesnik <andrei.kolesnik@gmail.com>
2018-10-16 14:56:44 -0700

    Escape a character in a string

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Data Structures and Algorithms interview questions and their solutions
 ## Arrays
-Different array handling methods are illustrated:
-* [C++] C-style array
-* [STL] Standard Library std::array
-* [NET] C# Array class
+* **[C++]** - C-style array
+* **[STL]** - Standard Library std::array
+* **[C#]** - C# Array class
 ### [Array.Binary-search](Array.Binary-search.CPP) [C++]
 Implement a binary search for the integer in a sorted array
 ### [Array.Find-pair](Array.Find-pair.NET) [C#]
@@ -14,3 +13,10 @@ Find a pair with the given sum in an unsorted array (sorting & iterators)
 Find a missing element in a randomized sequence
 ### [Array.Sorted-merge](Array.Sorted-merge.STL) [STL]
 Merge two sorted arrays
+## Strings
+* **[C++]** - C-style char[]
+* **[STL]** - Standard Library std::string
+* **[C#]** - C# String class
+### [String.Escape](String.Escape.CPP) [C++]
+Escape all % characters in a string with % as the escape character
+	
---
+++ String.Escape/String.Escape.CPP.cpp
@@ -0,0 +1,48 @@
+//Escape all % characters in a string with % as the escape character
+//IN:
+//A string with % characters
+//OUT:
+//The same string with all the % characters escaped with another % character
+//==============================================================================
+#include <stdio.h>
+#include <string.h>
+//ALTERNATIVE: #include <stdlib.h>
+//ALTERNATIVE: #include <malloc.h>
+using namespace std;
+//==============================================================================
+#define MAX_SIZE 80
+char* string_escape_cpp(char* str)
+{
+	//ALTERNATIVE: char *str_esc = (char*)malloc(2*MAX_SIZE+1);
+	static char str_esc[2*MAX_SIZE+1]; //static does not get destroyed after return; double size in case all the characters are "%"
+	int i = 0;
+	for (char *p = str; *p != 0; p++)
+	{ 
+		if (i > 2*MAX_SIZE) break;
+		if (*p == '%') str_esc[i++] = *p;
+		str_esc[i++] = *p;
+	}
+	str_esc[i] = 0;
+	return str_esc;
+}
+//==============================================================================
+#define NUM_STRS 7
+int main()
+{
+	char strs[NUM_STRS][MAX_SIZE] = {
+		"", //empty case
+		"Nothing to escape here",
+		"Almost 75% of all American college students have student loan debt",
+		"5% + 5% = 10%",
+		"set PATH=c:\\;%PATH%",
+		"SELECT * FROM table WHERE comment LIKE '%10%%%'",
+		"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" //extreme case
+	};
+
+	for (int i = 0; i < NUM_STRS; i++)
+	{
+		puts(string_escape_cpp(strs[i])); //safer and faster than printf()
+//		printf("%s [%d-%d]\n", string_escape_cpp(strs[i]), strlen(strs[i]), strlen(string_escape_cpp(strs[i])));
+	}
+	getchar();
+}
