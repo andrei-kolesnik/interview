63a1e8fce070a944aea9e516ac8121f96f178fe3
andrei-kolesnik <andrei.kolesnik@gmail.com>
2018-10-16 18:47:59 -0700

    Version with 2 character maps added

--- String.Anagram.STL/String.Anagram.STL.cpp
+++ String.Anagram.STL/String.Anagram.STL.cpp
@@ -26,8 +26,8 @@ bool string_anagram_stl(string str1, string str2)
 	return !string_sort_stl(str1).compare(string_sort_stl(str2)); //just compare both string when sorted
 }
 //==============================================================================
-//VERSION 2: using character map/count
-bool string_anagram_map(string str1, string str2)
+//VERSION 2a: using character map/count
+bool string_anagram_map1(string str1, string str2)
 {
 	unordered_map<char, int> chars;
 	for (char c : str1)
@@ -45,6 +45,22 @@ bool string_anagram_map(string str1, string str2)
 	return chars.size() == 0; //if the map ends up empty after all this - we have an anagram
 }
 //==============================================================================
+//VERSION 2b: using two character maps
+bool string_anagram_map2(string str1, string str2)
+{
+	unordered_map<char, int> chars1;
+	for (char c : str1)
+		if (c != ' ') 
+			chars1[c]++; 
+
+	unordered_map<char, int> chars2;
+	for (char c : str2)
+		if (c != ' ') 
+			chars2[c]++; 
+
+	return chars1 == chars2; //same character count in both strings - they are anagrams
+}
+//==============================================================================
 int main()
 {
 	array<array<string, 2>, 4> samples{ {
@@ -57,7 +73,8 @@ int main()
 	for (auto a : samples)
 	{
 		cout << a[0] << "|" << a[1] << " | " << (string_anagram_stl(a[0], a[1]) ? "Yes" : "No") << endl;
-		cout << a[0] << "|" << a[1] << " | " << (string_anagram_map(a[0], a[1]) ? "Yes" : "No") << endl;
+		cout << a[0] << "|" << a[1] << " | " << (string_anagram_map1(a[0], a[1]) ? "Yes" : "No") << endl;
+		cout << a[0] << "|" << a[1] << " | " << (string_anagram_map2(a[0], a[1]) ? "Yes" : "No") << endl;
 	}
 	getchar();
 }
